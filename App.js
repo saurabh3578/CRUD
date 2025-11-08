727ac6481d6a4967ed7e69a0ed147d1bc8e3689d
Saurabh Tapkir <saurabhtapkir3578@gmail.com>
2025-11-09 01:03:34 +0530

    Update component created successfully

--- App.js
+++ App.js
@@ -1,44 +1,52 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import Home from "./CRUD/Home"
-import Create from "./CRUD/Create"
-import Update from "./CRUD/Update"
-import View from "./CRUD/View"
-import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom/dist"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./CRUD/Home";
+import Create from "./CRUD/Create";
+import Update from "./CRUD/Update";
+import View from "./CRUD/View";
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom/dist";
 
-const App=()=>{
-    return(
-        <div>
-            <Outlet/>
-        </div>
-    )
-}
+const App = () => {
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
+};
 
-const appRouting=createBrowserRouter([
-    {
-        element:<App/>,
-        path:"/",
-        children:[
-            {
-                element:<Home/>,
-                path:"/"
-            },
-            {
-                element:<Create/>,
-                path:"/create"
-            },
-            {
-                element:<Update/>,
-                path:"/update/:userId"
-            },
-            {
-                element:<View/>,
-                path:"/view/:userId"
-            }
-        ],
-        errorElement:<h1>Page not found!</h1>
-    }
-])
+const appRouting = createBrowserRouter([
+  {
+    element: <App />,
+    path: "/",
+    children: [
+      {
+        element: <Home />,
+        path: "/",
+      },
+      {
+        element: <Create />,
+        path: "/create",
+      },
+      {
+        element: <Update />,
+        path: "/update/:userId",
+      },
+      {
+        element: <View />,
+        path: "/view/:userId",
+      },
+    ],
+    errorElement: <h1>Page not found!</h1>,
+  },
+]);
 
-const root=ReactDOM.createRoot(document.getElementById('root'))
-root.render(<RouterProvider router={appRouting}/>)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={appRouting} />
+  </React.StrictMode>
+);
--- CRUD/Update.jsx
+++ CRUD/Update.jsx
@@ -1,7 +1,67 @@
-const Update=()=>{
-    return(
+import axios from "axios"
+import { useEffect, useId, useState } from "react"
+import { useNavigate, useParams } from "react-router-dom/dist"
+
+const Update = () => {
+    const [id, setId] = useState("")
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const navigate = useNavigate()
+    const {userId}=useParams()
+
+    const fetchUser=async()=>{
+        const response=await axios.get("http://localhost:8000/Users/" + userId)
+        setId(response.data.id)
+        setName(response.data.name)
+        setEmail(response.data.email)
+    }
+
+    useEffect(()=>{
+        fetchUser()
+    },[userId])
+
+    const handleUpdate = (e) => {
+        e.preventDefault()
+        const userData = { id, name, email }
+        fetch("http://localhost:8000/Users/" + userId, {
+            method: "PUT",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(userData)
+        })
+            .then(() => {
+                alert("User Updated successfully!")
+                navigate("/")
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
+    return (
         <div>
-            <h1>This is update page</h1>
+            <h1>Update User</h1>
+            <form onSubmit={handleUpdate}>
+                <label htmlFor="">ID:</label>
+                <div>
+                    <input type="number" value={id} onChange={(e) => setId(e.target.value)} disabled/>
+                </div>
+
+                <label htmlFor="">Name:</label>
+                <div>
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} required/>
+                </div>
+
+                <label htmlFor="">Email:</label>
+                <div>
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+                </div>
+
+                <button type="submit">Update</button>
+                <button onClick={() => navigate("/")}>Back</button>
+
+            </form>
         </div>
     )
 }
