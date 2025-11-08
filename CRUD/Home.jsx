5a712eaf248e94590a32f05eb6aa903ed5bc9728
Saurabh Tapkir <saurabhtapkir3578@gmail.com>
2025-11-08 23:50:02 +0530

    Home page and db.json added with css for table

--- CRUD/Home.jsx
+++ CRUD/Home.jsx
@@ -1,7 +1,49 @@
-const Home=()=>{
-    return(
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+const Home = () => {
+    const [userList, setUserList] = useState([])
+
+    const fetchUser = async () => {
+        const response = await axios("http://localhost:8000/Users")
+        setUserList(response.data)
+    }
+
+    useEffect(() => {
+        fetchUser()
+    }, [])
+
+    console.log(userList)
+
+    return (
         <div>
-            <h1>This is home page</h1>
+            <button>Create</button>
+            <table>
+                <thead>
+                    <tr>
+                        <td>ID</td>
+                        <td>Name</td>
+                        <td>Email</td>
+                        <td>Action</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        userList.map((user) => (
+                            <tr key={user.id}>
+                                <td>{user.id}</td>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>
+                                    <button>View</button>
+                                    <button>Update</button>
+                                    <button>Delete</button>
+                                </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
         </div>
     )
 }
