43cb08ee3fae741883fcf814ff3b0a687d4141e3
Saurabh Tapkir <saurabhtapkir3578@gmail.com>
2025-11-08 23:34:51 +0530

    Routing Added

--- App.js
+++ App.js
@@ -1,13 +1,44 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import Home from "./CRUD/Home"
+import Create from "./CRUD/Create"
+import Update from "./CRUD/Update"
+import View from "./CRUD/View"
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom/dist"
 
 const App=()=>{
     return(
         <div>
-            <h1>This is applayout</h1>
+            <Outlet/>
         </div>
     )
 }
 
+const appRouting=createBrowserRouter([
+    {
+        element:<App/>,
+        path:"/",
+        children:[
+            {
+                element:<Home/>,
+                path:"/"
+            },
+            {
+                element:<Create/>,
+                path:"/create"
+            },
+            {
+                element:<Update/>,
+                path:"/update/:userId"
+            },
+            {
+                element:<View/>,
+                path:"/view/:userId"
+            }
+        ],
+        errorElement:<h1>Page not found!</h1>
+    }
+])
+
 const root=ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App/>)
+root.render(<RouterProvider router={appRouting}/>)
